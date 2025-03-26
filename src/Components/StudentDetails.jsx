397291abcb34577e7ca911a46e7a7e48a926b228
MD Jawad <122153153+Mo-Jawad@users.noreply.github.com>
2025-03-26 22:15:04 +0600

    Final touch done, behavior added

--- src/Components/StudentDetails.jsx
+++ src/Components/StudentDetails.jsx
@@ -46,13 +46,13 @@ function StudentDetails() {
   if (!dhainchas) {
     return <div>Loading...</div>;
   }
-  console.log(dhainchas);
+  console.log(dhainchas.behavior);
   console.log(dhainchas.image);
   return (
     <div className=' h-[35rem] w-full p-4 pt-8 bg-gray-700 items-center'>
       <div className='flex justify-between px-8'>
       <h2 className='font-bold text-white text-2xl tracking-widest'>{dhainchas.Name}'s Details</h2>
-      <Link className='p-3 px-5 hover:text-white transition-colors duration-200 bg-amber-400 rounded-md hover:bg-gray-700 hover:border-orange-400 border-2 font-bold border-gray-700' to="/">Home</Link>
+      <Link className='p-3 px-5 hover:text-white transition-colors duration-200 bg-amber-400 rounded-md hover:bg-gray-700 hover:border-orange-400 border-2 font-bold border-gray-700' to="/">Back To Home</Link>
       </div>
 
     <div className='w-full h-full pb-5'>
@@ -62,12 +62,22 @@ function StudentDetails() {
       </figure>
 
       <div className='w-2xl h-fit flex-col rounded-md shadow-md hover:shadow-green-600 text-center items-center justify-center p-5 py-8 text-2xl  font-bold text-orange-400 border-2 border-amber-400 bg-gray-800 transition duration-200'>
-      <div className='flex-col space-y-3 justify-center h-full'>
-      <h2>Name: {dhainchas.Name}</h2>
-      <h4>Home Town: {dhainchas.hometown}</h4>
-      <p>SSC: {dhainchas.SSC}</p>
-      <p>HSC: {dhainchas.HSC}</p>
-      {/* <p>{dhaincha.details}</p> */}
+      <div className='flex-col pl-4 space-y-3 justify-start text-start h-full'>
+      <h2>Name : {dhainchas.Name}</h2>
+      <h4>Home Town : {dhainchas.hometown}</h4>
+      <p>SSC : {dhainchas.SSC}</p>
+      <p>HSC : {dhainchas.HSC}</p>
+
+      { dhainchas.id !== 8 && (
+      <div className='flex-col items-start space-y-2 text-blue-300'>
+          <div className='w-full '>Behavior:</div>
+
+          <ul>
+            {dhainchas.behavior?.map((behave, index) => <div><li key={index}>  {`    ${index + 1}.  
+              ${behave}`}</li></div>)}
+          </ul>
+      </div> )
+      }
       </div>
       </div>
       </div>
